db278b7782927c163b991841f25a884bd7ceba37
StefanMihailovic <112586562+stefan-000@users.noreply.github.com>
2023-11-20 18:28:36 +0100

    Add calculator project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 // Add your JavaScript here
+
+function insert(number) {
+    document.getElementById('display').value =
+        document.getElementById('display').value + number;
+}
+function equalTo() {
+    let curr = document.getElementById('display').value;
+    if (curr) {
+        document.getElementById('display').value = eval(curr);
+    }
+}
+function clearAll() {
+    document.getElementById('display').value = '';
+}
+function deleteLastNumber() {
+    let curr = document.getElementById('display').value;
+    document.getElementById('display').value = curr.substring(
+        0,
+        curr.length - 1
+    );
+}
+
+function percentage() {
+    let curr = document.getElementById('display').value;
+    if (curr) {
+        document.getElementById('display').value = parseFloat(curr) / 100;
+    }
+}
